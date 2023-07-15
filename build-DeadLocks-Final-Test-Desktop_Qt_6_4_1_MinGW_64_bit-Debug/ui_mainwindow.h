a36de46d15a7236df85f0a37dbd04890bf818847
TimL03 <139620347+TimL03@users.noreply.github.com>
2023-07-15 14:11:08 +0200

    FirstUIAdded
    Added the UI with Icon, first Elements and removed run method

---
+++ build-DeadLocks-Final-Test-Desktop_Qt_6_4_1_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,190 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.4.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QIcon>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QLabel *C3_label_occupation;
+    QLabel *C1_label_occupation;
+    QLabel *B1_label_occupation;
+    QLabel *B2_label_occupation;
+    QLabel *A1_label_occupation;
+    QLabel *A3_label_occupation;
+    QLabel *B3_label_occupation;
+    QLabel *C2_label_occupation;
+    QLabel *B4_label_occupation;
+    QLabel *A2_label_occupation;
+    QLabel *C4_label_occupation;
+    QLabel *A4_label_occupation;
+    QLabel *Occupation_Matrix_Label;
+    QLabel *Printer_off_label;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(1314, 855);
+        QIcon icon;
+        icon.addFile(QString::fromUtf8(":/resources/Icon_White.png"), QSize(), QIcon::Normal, QIcon::Off);
+        MainWindow->setWindowIcon(icon);
+        MainWindow->setStyleSheet(QString::fromUtf8("background-color: rgb(52, 61, 85);"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        C3_label_occupation = new QLabel(centralwidget);
+        C3_label_occupation->setObjectName("C3_label_occupation");
+        C3_label_occupation->setGeometry(QRect(980, 640, 50, 42));
+        C3_label_occupation->setStyleSheet(QString::fromUtf8("color: rgb(217, 217, 217);\n"
+"font: 9pt \"Futura PT\";\n"
+"background-color: rgb(73, 81, 103);"));
+        C1_label_occupation = new QLabel(centralwidget);
+        C1_label_occupation->setObjectName("C1_label_occupation");
+        C1_label_occupation->setGeometry(QRect(860, 640, 50, 42));
+        C1_label_occupation->setStyleSheet(QString::fromUtf8("color: rgb(217, 217, 217);\n"
+"font: 9pt \"Futura PT\";\n"
+"background-color: rgb(73, 81, 103);"));
+        B1_label_occupation = new QLabel(centralwidget);
+        B1_label_occupation->setObjectName("B1_label_occupation");
+        B1_label_occupation->setGeometry(QRect(860, 592, 50, 41));
+        B1_label_occupation->setStyleSheet(QString::fromUtf8("color: rgb(217, 217, 217);\n"
+"font: 9pt \"Futura PT\";\n"
+"background-color: rgb(73, 81, 103);"));
+        B2_label_occupation = new QLabel(centralwidget);
+        B2_label_occupation->setObjectName("B2_label_occupation");
+        B2_label_occupation->setGeometry(QRect(920, 592, 51, 41));
+        B2_label_occupation->setStyleSheet(QString::fromUtf8("color: rgb(217, 217, 217);\n"
+"font: 9pt \"Futura PT\";\n"
+"background-color: rgb(73, 81, 103);"));
+        A1_label_occupation = new QLabel(centralwidget);
+        A1_label_occupation->setObjectName("A1_label_occupation");
+        A1_label_occupation->setGeometry(QRect(860, 543, 50, 42));
+        A1_label_occupation->setStyleSheet(QString::fromUtf8("color: rgb(217, 217, 217);\n"
+"font: 9pt \"Futura PT\";\n"
+"background-color: rgb(73, 81, 103);"));
+        A3_label_occupation = new QLabel(centralwidget);
+        A3_label_occupation->setObjectName("A3_label_occupation");
+        A3_label_occupation->setGeometry(QRect(980, 543, 50, 42));
+        A3_label_occupation->setStyleSheet(QString::fromUtf8("color: rgb(217, 217, 217);\n"
+"font: 9pt \"Futura PT\";\n"
+"background-color: rgb(73, 81, 103);"));
+        B3_label_occupation = new QLabel(centralwidget);
+        B3_label_occupation->setObjectName("B3_label_occupation");
+        B3_label_occupation->setGeometry(QRect(980, 592, 50, 41));
+        B3_label_occupation->setStyleSheet(QString::fromUtf8("color: rgb(217, 217, 217);\n"
+"font: 9pt \"Futura PT\";\n"
+"background-color: rgb(73, 81, 103);"));
+        C2_label_occupation = new QLabel(centralwidget);
+        C2_label_occupation->setObjectName("C2_label_occupation");
+        C2_label_occupation->setGeometry(QRect(920, 640, 51, 42));
+        C2_label_occupation->setStyleSheet(QString::fromUtf8("color: rgb(217, 217, 217);\n"
+"font: 9pt \"Futura PT\";\n"
+"background-color: rgb(73, 81, 103);"));
+        B4_label_occupation = new QLabel(centralwidget);
+        B4_label_occupation->setObjectName("B4_label_occupation");
+        B4_label_occupation->setGeometry(QRect(1036, 592, 52, 41));
+        B4_label_occupation->setStyleSheet(QString::fromUtf8("color: rgb(217, 217, 217);\n"
+"font: 9pt \"Futura PT\";\n"
+"background-color: rgb(73, 81, 103);"));
+        A2_label_occupation = new QLabel(centralwidget);
+        A2_label_occupation->setObjectName("A2_label_occupation");
+        A2_label_occupation->setGeometry(QRect(920, 543, 51, 42));
+        A2_label_occupation->setStyleSheet(QString::fromUtf8("color: rgb(217, 217, 217);\n"
+"font: 9pt \"Futura PT\";\n"
+"background-color: rgb(73, 81, 103);"));
+        C4_label_occupation = new QLabel(centralwidget);
+        C4_label_occupation->setObjectName("C4_label_occupation");
+        C4_label_occupation->setGeometry(QRect(1036, 640, 50, 42));
+        C4_label_occupation->setStyleSheet(QString::fromUtf8("color: rgb(217, 217, 217);\n"
+"background-color: rgb(73, 81, 103);\n"
+"font: 9pt \"Futura PT\";"));
+        A4_label_occupation = new QLabel(centralwidget);
+        A4_label_occupation->setObjectName("A4_label_occupation");
+        A4_label_occupation->setGeometry(QRect(1036, 543, 50, 42));
+        A4_label_occupation->setStyleSheet(QString::fromUtf8("color: rgb(217, 217, 217);\n"
+"font: 9pt \"Futura PT\";\n"
+"background-color: rgb(73, 81, 103);"));
+        Occupation_Matrix_Label = new QLabel(centralwidget);
+        Occupation_Matrix_Label->setObjectName("Occupation_Matrix_Label");
+        Occupation_Matrix_Label->setGeometry(QRect(654, 397, 591, 371));
+        Occupation_Matrix_Label->setStyleSheet(QString::fromUtf8("image: url(:/resources/Occupation_Matrix.png);"));
+        Printer_off_label = new QLabel(centralwidget);
+        Printer_off_label->setObjectName("Printer_off_label");
+        Printer_off_label->setGeometry(QRect(90, 430, 171, 131));
+        Printer_off_label->setStyleSheet(QString::fromUtf8("image: url(:/resources/Printer_off.png);"));
+        MainWindow->setCentralWidget(centralwidget);
+        Occupation_Matrix_Label->raise();
+        C3_label_occupation->raise();
+        C1_label_occupation->raise();
+        B1_label_occupation->raise();
+        B2_label_occupation->raise();
+        A1_label_occupation->raise();
+        A3_label_occupation->raise();
+        B3_label_occupation->raise();
+        C2_label_occupation->raise();
+        B4_label_occupation->raise();
+        A2_label_occupation->raise();
+        C4_label_occupation->raise();
+        A4_label_occupation->raise();
+        Printer_off_label->raise();
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 1314, 31));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "Deadlock Algorithm Simulation", nullptr));
+        C3_label_occupation->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        C1_label_occupation->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        B1_label_occupation->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        B2_label_occupation->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        A1_label_occupation->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        A3_label_occupation->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        B3_label_occupation->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        C2_label_occupation->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        B4_label_occupation->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        A2_label_occupation->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        C4_label_occupation->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        A4_label_occupation->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        Occupation_Matrix_Label->setText(QString());
+        Printer_off_label->setText(QString());
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H

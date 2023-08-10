5427c5673f9a307975027b9c3506d824254e81b5
TimL03 <139620347+TimL03@users.noreply.github.com>
2023-08-10 15:29:48 +0200

    added Doxygen for holdandwait

--- DeadLocks-Final-Test/Algorithms/eliminateholdandwait.cpp
+++ DeadLocks-Final-Test/Algorithms/eliminateholdandwait.cpp
@@ -10,7 +10,6 @@ EliminateHoldAndWait::EliminateHoldAndWait()
 
 }
 
-
 QList<int> EliminateHoldAndWait::findNextResource(SystemProcess process, int stillNeededResources_R[3][4], int assignedResources_C[3][4], int differenceResources_A[4], int availableResources_E[4]){
 
     QList<int> result;
@@ -22,37 +21,36 @@ QList<int> EliminateHoldAndWait::findNextResource(SystemProcess process, int sti
     //indexResourceList is the index of the nextResource in the neededResources list of process as they are in random order
     int indexResourceList = -1;
 
+    //if this is the first call set up the copy of the differenceResources because it has to be changed seperatly
     if(currentProcess.empty()){
+        //flag as not empty
         currentProcess.append(-1);
         for(int i = 0; i < 4; i++){
             copyDifference[i] = differenceResources_A[i];
         }
-        qDebug() << "copy";
     }
 
-    //Step 4: Get nextResource
+    //test if it hasn't requested anything yet
     if(!currentProcess.contains(process.getProcessId())){
+        //try to acquire a semaphore
         semaphore->acquire(1);
+        //if everything is free we can continue to reserve
         if(avoidance_algorithm(process, copyDifference)){
+            //add process to currentProcess to show that it has now requested its resources
             currentProcess.append(process.getProcessId());
-            qDebug() << "process" << process.getName() << "acquired" << "id:" << process.getProcessId();
-            qDebug() << "needed: " << process.getNeededResources().at(0).getCount() << process.getNeededResources().at(1).getCount() << process.getNeededResources().at(2).getCount() << process.getNeededResources().at(3).getCount();
-            qDebug() << "lastDifference" << copyDifference[0] << copyDifference[1] << copyDifference[2] << copyDifference[3];
+
+            //update copyDifference Array with all the counts of the current process
             for(int i = 0; i < 4; i++){
-                qDebug() << "differences set" << copyDifference[process.getNeededResources().at(i).getResourceId()] <<  "-" << process.getNeededResources().at(i).getCount();
                 copyDifference[process.getNeededResources().at(i).getResourceId()] -= process.getNeededResources().at(i).getCount();
             }
-            //qDebug() << "differences set" << copyDifference[0] << copyDifference[1] << copyDifference[2] << copyDifference[3];
 
+            //if there are still free resources then another process can try to reserve some
             if(copyDifference[0] > 0 || copyDifference[1] > 0 || copyDifference[2] > 0 || copyDifference[3] > 0){
-                qDebug() << "release" << copyDifference[0] << copyDifference[1] << copyDifference[2] << copyDifference[3];
                 semaphore->release(1);
             }
 
-        }  else {
+        }  else { //not enough free, return to retry at a later point
             semaphore->release(1);
-            qDebug() << "process: " << process.getName() << " on pause";
-            qDebug() << "because: " << copyDifference[0] << copyDifference[1] << copyDifference[2] << copyDifference[3];
             nextResource = -2;
             result.append(nextResource);
             result.append(countResource);
@@ -61,83 +59,48 @@ QList<int> EliminateHoldAndWait::findNextResource(SystemProcess process, int sti
         }
     }
 
+    //going through the neededResources list to find the next needed resource
+    for(int i = 0; i < process.getNeededResources().count(); i++){
+        //the resource has to have a count > 0 but < the over all available resources
+        if(process.getNeededResources().at(i).getCount() <= availableResources_E[process.getNeededResources().at(i).getResourceId()] && process.getNeededResources().at(i).getCount() > 0){
+            //if the next resource was found set the nextResource, countResource and indexResourceList variables
+            nextResource = process.getNeededResources().at(i).getResourceId();
+            indexResourceList = i;
+            countResource = process.getNeededResources().at(i).getCount();
+            //update copyDifference
+            copyDifference[nextResource] += countResource;
+            break;
 
-        qDebug() << "process: " << process.getName() << " save" << currentProcess;
-
-
-        //memset(differenceResources_A, 0, 4);
-        //going through the neededResources list to find the next needed resource
-
-
-
-        for(int i = 0; i < process.getNeededResources().count(); i++){
-            //the resource has to have a count > 0 but < the over all available resources
-            if(process.getNeededResources().at(i).getCount() <= availableResources_E[process.getNeededResources().at(i).getResourceId()] && process.getNeededResources().at(i).getCount() > 0){
-
-                //if the next resource was found set the nextResource, countResource and indexResourceList variables
-                nextResource = process.getNeededResources().at(i).getResourceId();
-                indexResourceList = i;
-                countResource = process.getNeededResources().at(i).getCount();
-                copyDifference[nextResource] += countResource;
-                break;
-
-            } else if(i == process.getNeededResources().count() - 1 && nextResource == -1){
-                //in this case there are no resources left to process (all are either 0 or can't be processed because they exeed the over all available resource count
-                //no return yet because last resource has to be released
-                nextResource = - 5;
-                for(int i = 0; i < 4; i++){
-                    //copyDifference[process.getNeededResources().at(i).getResourceId()] += process.getNeededResources().at(i).getCount();
-                }
-                break;
-            }
+        } else if(i == process.getNeededResources().count() - 1 && nextResource == -1){
+            //in this case there are no resources left to process (all are either 0 or can't be processed because they exeed the over all available resource count
+            //no return yet because last resource has to be released
+            nextResource = - 5;
+            break;
         }
+    }
 
+    result.append(nextResource);
+    result.append(countResource);
+    result.append(indexResourceList);
 
-
-        result.append(nextResource);
-        result.append(countResource);
-        result.append(indexResourceList);
-
-
-
-        //Step 6: Return the list of Values
-        qDebug() << "Result: Process " << process.getProcessId() << " reserved Resource " << nextResource;
-
-        if(nextResource == -5){
-           qDebug() << "unlocked";
-           semaphore->release(1);
-        }
-
-
+    //if there are no resources left to reserve a semaphore will be released
+    if(nextResource == -5){
+       qDebug() << "unlocked";
+       semaphore->release(1);
+    }
 
     return result;
 }
 
-bool EliminateHoldAndWait::avoidance_algorithm(SystemProcess process, int differenceResources_A[4])
+bool EliminateHoldAndWait::avoidance_algorithm(SystemProcess process, int copyDifference[4])
 {
-    //Steop 2: Check if enough Resources are available. If NOT -> Step 5
-    bool allConditionsMet = true;
+    bool resourcesAvailable = true;
+    //Check if all needed Resources are available
     for(int i = 0 ; i < process.getNeededResources().count(); i++){
-        if(process.getNeededResources().at(i).getCount() > differenceResources_A[process.getNeededResources().at(i).getResourceId()]){
-            allConditionsMet = false;
+        if(process.getNeededResources().at(i).getCount() > copyDifference[process.getNeededResources().at(i).getResourceId()]){
+            resourcesAvailable = false;
         }
     }
 
-    //Step 3: Check No-Preemption. Yes -> Step 4 , No -> Step 5
-    /*if(allConditionsMet){
-
-        //If the algorithm No-Preemption is true, the next resource can be assigned
-        NoPreemption nopreemption = *new NoPreemption();
-
-        //If the Result, the No-Preemption algorithm is giving equals our expectations
-        nopreemption.avoidance_algorithm(copyNeededResources);
-        //ToDo .. check no preemption status...
-
-        bool checkNoPreemption = true;
-        if(!checkNoPreemption){
-            allConditionsMet = false;
-        }
-    }*/
-
-    return allConditionsMet;
+    return resourcesAvailable;
 }
